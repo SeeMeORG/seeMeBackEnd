b500cc6dd3845c1fb62382dc3349c3bb89d9bfb2
sky1412001 <akashjaggi1123@gmail.com>
2025-04-07 22:21:14 +0530

    Socket connection Setup

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  console.log('Client connected');
+
+  ws.on('message', function incoming(message) {
+    console.log('Received:', message);
+
+    // Echo message back to client
+    ws.send(`Server received: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+
+  ws.send('Welcome to WebSocket server!');
+});
+
+console.log('WebSocket server running at ws://localhost:8080');
